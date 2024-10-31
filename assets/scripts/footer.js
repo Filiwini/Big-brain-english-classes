bf29ed724f87bb48a1e6663af4e5c3e86bd93606
Filiwini <f.krolewski@wp.pl>
2024-10-31 18:17:32 +0100

    Add files via upload

---
+++ assets/scripts/footer.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const web_name_button = document.querySelector("#web_name_in_footer");
+  web_name_button.addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
+});
